ba32363bd9e3f676cdf6426f09cb7428a8944365
danielbadry <danielbadry@gmail.com>
2024-06-09 09:06:15 +0330

    feat: add customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'firstname', 'lastname', 'date_of_birth', 'phone_number', 'email', 'bank_account_number'
+    ];
+
+    public static $rules = [
+        'firstname' => 'required',
+        'lastname' => 'required',
+        'date_of_birth' => 'required|date',
+        'phone_number' => 'required|unique:customers,phone_number',
+        'email' => 'required|email|unique:customers,email',
+        'bank_account_number' => 'required|unique:customers,bank_account_number',
+    ];
 }
